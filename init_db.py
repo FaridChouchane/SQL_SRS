999f6746cdc7a7d7a17b5c614722d1064978b1c3
root <root@Fa>
2024-12-21 15:27:01 +0100

    init db on st2

--- init_db.py
+++ init_db.py
@@ -64,3 +64,6 @@ trademarks = pd.read_csv(io.StringIO(trademarks))
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 con.execute("CREATE TABLE IF NOT EXISTS trademarks AS SELECT * FROM trademarks")
+
+con.close()
+
